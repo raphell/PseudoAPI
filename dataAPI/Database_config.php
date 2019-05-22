6b563012adc8ec595ce6a2e69e1550f76f44a6ba
White Jacques <32514062+NathanTraineau@users.noreply.github.com>
2019-05-22 19:08:51 +0200

    changement BD

--- dataAPI/Database_config.php
+++ dataAPI/Database_config.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$Database_Host = "localhost";
-$Database_Name = "dump";
-$Database_Username = "root";
-$Database_password ="";
+$Database_Host = "ec2-54-75-239-237.eu-west-1.compute.amazonaws.com";
+$Database_Name = "d2rk6tkj78feg0";
+$Database_Username = "iriahouobgkecq";
+$Database_password ="5037afca7ecac05e3531167064f38d1eab682035d85cfdfcf2add440c2f537e3";
 
 ?>
